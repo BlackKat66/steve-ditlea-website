1d4fb00f6e1ebd889ce869086e82a970aac19040
steveditleanet <steveditleanet@gmail.com>
2023-04-04 12:56:59 -0400

    added back content for home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,58 @@
 const Home = () => (
-  <div>
-    This is the home page.
-  </div>
+  <section className="page-section bg-primary text-black mb-0 text-black" id="about">
+    <div className="container">
+      <h2 className="page-section-heading text-center text-uppercase text-black">Home</h2>
+      <div className="divider-custom divider-light text-black">
+        <div className="divider-custom-line"></div>
+        <div className="divider-custom-icon"><i className="fas fa-house-user" style={{fontSize: "3.5 rem"}} /></div>
+        <div className="divider-custom-line"></div>
+      </div>
+
+      <div className="row">
+        <div className="col-lg-6 mr-auto">
+
+          <p className="lead">Most widely-published freelance print & digital journalist you've never heard of, proud of it.
+          </p>
+          <p className="lead">
+
+            Hard-boiled reporter and editor, dedicated to clarity, accuracy and fun for any audience.
+            Zelig Gump Ditlea has interacted with John Wayne, Stevie Wonder, Margaret Mead, Danielle Steel (nee
+            Schulein, in elementary school together), W. Edwards Deming, Ted Richards, John Gabree, Stephen Manes, Joan
+            Baez, Dennis Quaid, Tim Berners-Lee, Mayor David Dinkins, KISS, Denny O'Neil, J. Presper Eckert, "cowboy"
+            "poet" John Perry Barlow, Rita Hayworth, Willie Nelson, John Van Dusen Lewis, Woody Lewis, Peter Lewis,
+            Shari Lewis, Maggie Kuhn, Tom Paxton, Timothy Leary (close personal friend in later years), Cheech Marin,
+            Bob Dylan, Sam Peckinpah, Kris Kristofferson, Rita Coolidge, David Lehman, David Shapiro, Kenneth Koch, Paul
+            Weintraub, Phil Proctor, Marvin Minsky, Jerry Wexler, National Lampoon Crew (P.J. O'Rourke, Mr. Mike, Jeff
+            Greenfield), Andrew Sarris, Bob Marley, Jim McBride, Daniel Bell, Aretha Franklin, L. M. "Kit" Carson, Karen
+            Black, Hunter Carson, Peter Max, Ray Bradbury, Jaron Lanier, Elton John, Bob Guccione Jr., Joyce Wadler,
+            Susan Berman (only murder victim I've known), Bill Gates, Alice Cooper, Brian De Palma (what an a-hole),
+            Steve Jobs (what an a-hole), Jimi Hendrix, what a nice guy, Charles de Gaulle, Yoko Ono, Andy Warhol, Chris
+            Frantz, Stan Lee (what an a-hole), Henny Youngman (meh), Mr. James Brown, Frank Capra, Jerry Nadler, Rev. Al
+            Sharpton, Fleetwood Mac, Marty Scorsese, SHA NA NA, Don Johnson. Melanie Griffith, Richard Pryor--sounds
+            like "We Didn't Start The Fire"?--oh yeah, Billy Joel!
+          </p>
+        </div>
+        <div className="col-lg-6 mr-auto">
+          <p className="lead">Technology reporter for Inc., Upside, Popular Mechanics, Scientific American, Technology
+            Review, et al.</p>
+
+          <p className="lead">Music maven for Penthouse, Rolling Stone, Circus, Country Music, New Ingenue...</p>
+
+          <p className="lead">Scriptwriter, lecturer.</p>
+
+          <p className="lead">Specialties: Mature big-picture understanding, personality-driven science writing, boldfaced
+            names coverage, health and medical developments, technology trends and uses, business sagas and start-up
+            tales, computer history, virtual reality, technology marketing, consumer electronics, product testing and
+            reviewing, popular culture, movies, music of the 60s, 70s, 80s, 90s, 00s, 2010-2021 not so much, drop-dead
+            one-liners, black puma-like cats, especially Sheena, who could have been a punk rocker...
+          </p>
+          <p className="text-center"><img src="assets/sheena_cats_small.png" width="180" alt="" className="img-fluid" />
+
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
 );
 
 export default Home; 
