cb2ba2e0cbac1c71c687722b2d7342617d82be94
steveditleanet <steveditleanet@gmail.com>
2023-04-04 14:45:37 -0400

    built basic photos page

--- app/photos/page.tsx
+++ app/photos/page.tsx
@@ -1,5 +1,18 @@
 const Photos = () => (
-  <div>This is the photos page.</div>
+  <section className="page-section portfolio" id="photos" style={{marginTop: "-3rem"}}>
+    <div className="container">
+      <h2 className="page-section-heading text-center text-uppercase text-secondary mb-0">Photos</h2>
+      <div className="divider-custom">
+        <div className="divider-custom-line"></div>
+        <div className="divider-custom-icon"><i className="far fa-book-reader" style={{fontSize: "3.5rem"}}></i></div>
+        <div className="divider-custom-line"></div>
+      </div>
+      <div>
+        Here's where the photos will go. 
+      </div>
+    </div>
+
+  </section>
 );
 
 export default Photos; 
