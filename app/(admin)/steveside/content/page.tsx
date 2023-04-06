71b63796fd002f0187f6c91430648d1cb9211831
steveditleanet <steveditleanet@gmail.com>
2023-04-06 11:51:23 -0400

    styled tabs

--- app/(admin)/steveside/content/page.tsx
+++ app/(admin)/steveside/content/page.tsx
@@ -1,16 +1,27 @@
 "use client";
 
-import { Tab, Tabs } from "react-bootstrap";
+import { Container, Row, Tab, Tabs } from "react-bootstrap";
+
+const tabStyles = {
+  marginTop: "20px",
+  borderRadius: "4px", 
+  padding: "10px"
+};
 
 const AdminContent = () => (
-  <Tabs defaultActiveKey={"articles"}>
-    <Tab eventKey="articles" title="Articles" >
-      Upload articles here.
-    </Tab>
-    <Tab eventKey="photos" title="Photos">
-      Upload photos here.
-    </Tab>
-  </Tabs>
-); 
+  <Container>
+    <Row  className="justify-content-center" style={{border: "1px solid black", ...tabStyles}}>
+      <Tabs defaultActiveKey={"articles"} style={{padding: "20px"}}>
+        <Tab  eventKey="articles"  title="Articles" style={{ ...tabStyles}}>
+          Upload articles here.
+        </Tab>
+        <Tab eventKey="photos" title="Photos" style={{...tabStyles}}>
+          Upload photos here.
+        </Tab>
+      </Tabs>
+    </Row>
+  </Container>
+
+);
 
 export default AdminContent;
