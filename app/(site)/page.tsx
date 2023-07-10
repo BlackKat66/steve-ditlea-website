c23a80ebd549d5ca0b906e5074b99da63ef30802
steveditleanet <steveditleanet@gmail.com>
2023-07-09 23:55:01 -0400

    loading all home page content via contentful api call

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,58 +1,33 @@
 import Image from "next/image";
 import Divider from "./components/divider";
+import { getHomePageCopy } from "../utils";
 
-const Home = () => (
-  <section className="page-section bg-primary text-black mb-0 text-black" id="about" style={{marginTop: "-3rem"}}>
-    <div className="container">
-      <Divider iconClass="fas fa-house-user" title="Home" />
-      <div className="row">
-        <div className="col-lg-6 mr-auto">
-          <p className="lead">
-            {`Most widely-published freelance print & digital journalist you've never heard of, proud of it.`}
-          </p>
-          <p className="lead">
-            {`
-              Hard-boiled reporter and editor, dedicated to clarity, accuracy and fun for any audience.
-              Zelig Gump Ditlea has interacted with John Wayne, Stevie Wonder, Margaret Mead, Danielle Steel (nee
-              Schulein, in elementary school together), W. Edwards Deming, Ted Richards, John Gabree, Stephen Manes, Joan
-              Baez, Dennis Quaid, Tim Berners-Lee, Mayor David Dinkins, KISS, Denny O'Neil, J. Presper Eckert, "cowboy"
-              "poet" John Perry Barlow, Rita Hayworth, Willie Nelson, John Van Dusen Lewis, Woody Lewis, Peter Lewis,
-              Shari Lewis, Maggie Kuhn, Tom Paxton, Timothy Leary (close personal friend in later years), Cheech Marin,
-              Bob Dylan, Sam Peckinpah, Kris Kristofferson, Rita Coolidge, David Lehman, David Shapiro, Kenneth Koch, Paul
-              Weintraub, Phil Proctor, Marvin Minsky, Jerry Wexler, National Lampoon Crew (P.J. O'Rourke, Mr. Mike, Jeff
-              Greenfield), Andrew Sarris, Bob Marley, Jim McBride, Daniel Bell, Aretha Franklin, L. M. "Kit" Carson, Karen
-              Black, Hunter Carson, Peter Max, Ray Bradbury, Jaron Lanier, Elton John, Bob Guccione Jr., Joyce Wadler,
-              Susan Berman (only murder victim I've known), Bill Gates, Alice Cooper, Brian De Palma (what an a-hole),
-              Steve Jobs (what an a-hole), Jimi Hendrix, what a nice guy, Charles de Gaulle, Yoko Ono, Andy Warhol, Chris
-              Frantz, Stan Lee (what an a-hole), Henny Youngman (meh), Mr. James Brown, Frank Capra, Jerry Nadler, Rev. Al
-              Sharpton, Fleetwood Mac, Marty Scorsese, SHA NA NA, Don Johnson. Melanie Griffith, Richard Pryor--sounds
-              like "We Didn't Start The Fire"?--oh yeah, Billy Joel!
-            `}
-          </p>
-        </div>
-        <div className="col-lg-6 mr-auto">
-          <p className="lead">
-            {`Technology reporter for Inc., Upside, Popular Mechanics, Scientific American, Technology
-            Review, et al.`}
-          </p>
-          <p className="lead">
-            {`Music maven for Penthouse, Rolling Stone, Circus, Country Music, New Ingenue...`}
-          </p>
-          <p className="lead">{`Scriptwriter, lecturer.`}</p>
-          <p className="lead">
-            {`Specialties: Mature big-picture understanding, personality-driven science writing, boldfaced
-            names coverage, health and medical developments, technology trends and uses, business sagas and start-up
-            tales, computer history, virtual reality, technology marketing, consumer electronics, product testing and
-            reviewing, popular culture, movies, music of the 60s, 70s, 80s, 90s, 00s, 2010-2021 not so much, drop-dead
-            one-liners, black puma-like cats, especially Sheena, who could have been a punk rocker...`}
-          </p>
-          <p className="text-center">
-            <Image src="/sheena_cats_small.png" height={180} width={180} alt="picture of a black cat" className="img-fluid" />
-          </p>
+const Home = async () => {
+  const homePageCopyData = await getHomePageCopy(); 
+
+  const { introBlurb, listOfNames, rightColumn } = homePageCopyData[0].fields;
+
+  const parsedRightColumn = String(rightColumn).split('\n\n');
+
+  return (
+    <section className="page-section bg-primary text-black mb-0 text-black" id="about" style={{marginTop: "-3rem"}}>
+      <div className="container">
+        <Divider iconClass="fas fa-house-user" title="Home" />
+        <div className="row">
+          <div className="col-lg-6 mr-auto">
+            <p className="lead">{ String(introBlurb) }</p>
+            <p className="lead"> {String(listOfNames)}</p>
+          </div>
+          <div className="col-lg-6 mr-auto">
+            {parsedRightColumn.map((item, index) => <p className="lead" key={`p-${index}`}>{item}</p>)}
+            <p className="text-center">
+              <Image src="/sheena_cats_small.png" height={180} width={180} alt="picture of a black cat" className="img-fluid" />
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};  
 
 export default Home; 
---
+++ app/utils.ts
@@ -0,0 +1,14 @@
+import { createClient } from 'contentful';
+
+export const contentfulClient = createClient({
+  space: process.env.CONTENTFUL_SPACE as string, 
+  accessToken: process.env.CONTENTFUL_TOKEN as string
+});
+
+export const getHomePageCopy = async () => {
+  const response = await contentfulClient.getEntries({
+    content_type: "homePage",
+  });
+
+  return response.items;
+};
