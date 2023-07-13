537a255b3eddc47e23345baad8f9f6cff224370f
steveditleanet <steveditleanet@gmail.com>
2023-07-13 11:24:27 -0400

    triggering deployment

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -24,6 +24,7 @@ const Home = async () => {
               <Image src="/sheena_cats_small.png" height={180} width={180} alt="picture of a black cat" className="img-fluid" />
             </p>
             <p className="lead">{ String(bottomBlurb) }</p>
+            <p>Test</p>
           </div>
         </div>
       </div>
