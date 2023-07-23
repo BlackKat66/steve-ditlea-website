8ef716f468e024a1d0e964c8f0a92040feefb2e2
steveditleanet <steveditleanet@gmail.com>
2023-07-23 16:35:54 -0400

    further clarification via comment

--- app/(site)/stories/page.tsx
+++ app/(site)/stories/page.tsx
@@ -1,8 +1,9 @@
 // This page is called "Stories," but individual pieces of content fetched from Contentful here
 // are called "articles". This might be a bit confusing re: namespacing, but is the result of a
-// last-minute editorial change by Steve near the end of me working on the project. I didn't have
-// sufficient time to correct this at the API level on Contentful, so I changed Articles to Stories
-// in all places that would be visible to the user and left everything else as-is. 
+// last-minute editorial change by Steve near the end of me working on the project - he wanted me
+// to switch "Articles" to "Stories". I didn't have sufficient time to correct this at the API level
+// on Contentful, so I changed Articles to Stories in all places that would be visible to the user 
+// and left everything else as-is with the pre-existing namespacing. 
 
 import { format, parseISO } from "date-fns";
 
