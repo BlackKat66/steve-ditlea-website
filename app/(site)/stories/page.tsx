6de7519da8fe3af7a6ebb6a443dc6ee3f4681165
steveditleanet <steveditleanet@gmail.com>
2023-07-23 19:38:12 -0400

    fixed date parsing issue; cleaned up dependencies

--- app/(site)/stories/page.tsx
+++ app/(site)/stories/page.tsx
@@ -5,7 +5,7 @@
 // on Contentful, so I changed Articles to Stories in all places that would be visible to the user 
 // and left everything else as-is with the pre-existing namespacing. 
 
-import { format, parseISO } from "date-fns";
+import { DateTime } from "luxon";
 
 import Article from './article';
 import Divider from '../components/divider';
@@ -14,17 +14,21 @@ import { getArticles } from '@/app/utils';
 const Stories = async () => {
   const articles = await getArticles();
 
+  console.log({ rawPublicationDates: articles.map((item) => item.fields.publicationDate) });
+
   const parsedArticles = articles.map((item: any) => ({
     headline: item.fields.headline as string,
     description: item.fields.description || "" as string,
     publishedBy: item.fields.publishedBy as string,
     // @ts-ignore
-    publicationDate: format(parseISO(item.fields.publicationDate), "MMMM d, y"),
+    publicationDate: DateTime.fromISO(item.fields.publicationDate.split("T")[0]).toFormat("MMMM d, y"),
     // @ts-ignore
     articleLink: item.fields.article.fields.file.url as string,
     // @ts-ignore
     thumbnailLink: item.fields.thumbnail ? item.fields.thumbnail.fields.file.url as string : undefined
   }));
+
+  console.log(parsedArticles); 
 
   return (
     <section className="page-section portfolio" id="articles" style={{marginTop: "-3rem"}}>
