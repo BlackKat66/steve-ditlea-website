af1b366df5554f6e31f86da322018e88fb22f955
steveditleanet <steveditleanet@gmail.com>
2023-07-30 11:37:45 -0400

    revised credits

--- app/(site)/books/page.tsx
+++ app/(site)/books/page.tsx
@@ -1,5 +1,4 @@
 
-import Image from "next/image";
 import Divider from "../components/divider";
 
 const Books = () => (
--- app/(site)/components/footer.tsx
+++ app/(site)/components/footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => (
     <div className="copyright py-4 text-center text-white">
     <div className="container text-medium">
         Copyright © 2021 Steve Ditlea <br />
-        <a href="http://tedrichards.net/" target="new">Web site design and development by Ted Richards</a>
+        <a href="http://tedrichards.net/" target="new">Web site design by Ted Richards</a>
     </div>
   </div>
   </>
