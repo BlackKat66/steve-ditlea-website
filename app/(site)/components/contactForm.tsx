5594b903e67002bbd452cdef224bf217b4a94be4
steveditleanet <steveditleanet@gmail.com>
2023-07-30 12:08:26 -0400

    contact form works with useState

--- app/(site)/components/contactForm.tsx
+++ app/(site)/components/contactForm.tsx
@@ -1,61 +1,124 @@
-import Script from "next/script";
+"use client";
 
-const ContactForm = () => (
-  <>
-    <Script src="./scripts/contact_me.js" />
-    <Script src="./scripts/jqBootstrapValidation.js" />
-    <section className="page-section" id="contact">
-      <div className="container">
-        <h2 className="page-section-heading text-center text-uppercase text-secondary mb-0">Contact Me</h2>
-        <div className="divider-custom">
-          <div className="divider-custom-line"></div>
-          <div className="divider-custom-icon"><i className="fas fa-envelope-open-text" style={{ fontSize: "3.5 rem" }}></i></div>
-          <div className="divider-custom-line"></div>
-        </div>
-        <div className="row">
-          <div className="col-lg-8 mx-auto">
-            <form id="contactForm" name="sentMessage" noValidate>
-              <div className="control-group">
-                <div className="form-group floating-label-form-group controls mb-0 pb-2">
-                  <label>Name</label>
-                  <input className="form-control" id="name" type="text" placeholder="Name" required
-                    data-validation-required-message="Please enter your name." />
-                  <p className="help-block text-danger"></p>
+import { useState } from "react";
+
+const ContactForm = () => { 
+  const [ inputs, setInputs ] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    message: ""
+  }); 
+
+  const handleChange = (e: React.ChangeEvent) => {
+    const target = e.target as HTMLInputElement;
+
+		setInputs((prev) => ({
+			...prev,
+			[e.target.id]: target.value,
+		}))
+	};
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault(); 
+
+    console.log({ inputs }); 
+
+    setInputs({
+      name: "",
+      email: "",
+      phone: "",
+      message: ""
+    }); 
+  }
+
+  return (
+      <section className="page-section" id="contact">
+        <div className="container">
+          <h2 className="page-section-heading text-center text-uppercase text-secondary mb-0">Contact Me</h2>
+          <div className="divider-custom">
+            <div className="divider-custom-line"></div>
+            <div className="divider-custom-icon"><i className="fas fa-envelope-open-text" style={{ fontSize: "3.5 rem" }}></i></div>
+            <div className="divider-custom-line"></div>
+          </div>
+          <div className="row">
+            <div className="col-lg-8 mx-auto">
+              <form id="contactForm" name="sentMessage" onSubmit={handleSubmit}>
+                <div className="control-group">
+                  <div className="form-group floating-label-form-group controls mb-0 pb-2">
+                    <label>Name</label>
+                    <input 
+                      className="form-control" 
+                      id="name" 
+                      type="text" 
+                      placeholder="Name" 
+                      value={inputs.name}
+                      onChange={handleChange}
+                      required
+                      data-validation-required-message="Please enter your name." />
+                    <p className="help-block text-danger"></p>
+                  </div>
                 </div>
-              </div>
-              <div className="control-group">
-                <div className="form-group floating-label-form-group controls mb-0 pb-2">
-                  <label>Email Address</label>
-                  <input className="form-control" id="email" type="email" placeholder="Email Address" required
-                    data-validation-required-message="Please enter your email address." />
-                  <p className="help-block text-danger"></p>
+                <div className="control-group">
+                  <div className="form-group floating-label-form-group controls mb-0 pb-2">
+                    <label>Email Address</label>
+                    <input 
+                      className="form-control" 
+                      id="email" 
+                      type="email"
+                      placeholder="Email Address" 
+                      value={inputs.email}
+                      onChange={handleChange}
+                      required
+                      data-validation-required-message="Please enter your email address." />
+                    <p className="help-block text-danger"></p>
+                  </div>
                 </div>
-              </div>
-              <div className="control-group">
-                <div className="form-group floating-label-form-group controls mb-0 pb-2">
-                  <label>Phone Number</label>
-                  <input className="form-control" id="phone" type="tel" placeholder="Phone Number" required
-                    data-validation-required-message="Please enter your phone number." />
-                  <p className="help-block text-danger"></p>
+                <div className="control-group">
+                  <div className="form-group floating-label-form-group controls mb-0 pb-2">
+                    <label>Phone Number</label>
+                    <input 
+                      className="form-control" 
+                      id="phone" 
+                      type="tel" 
+                      value={inputs.phone}
+                      onChange={handleChange}
+                      placeholder="Phone Number" 
+                    />
+                    <p className="help-block text-danger"></p>
+                  </div>
                 </div>
-              </div>
-              <div className="control-group">
-                <div className="form-group floating-label-form-group controls mb-0 pb-2">
-                  <label>Message</label>
-                  <textarea className="form-control" id="message" rows={5} placeholder="Message" required
-                    data-validation-required-message="Please enter a message."></textarea>
-                  <p className="help-block text-danger"></p>
+                <div className="control-group">
+                  <div className="form-group floating-label-form-group controls mb-0 pb-2">
+                    <label>Message</label>
+                    <textarea 
+                      className="form-control" 
+                      id="message" 
+                      rows={5} 
+                      placeholder="Message" 
+                      value={inputs.message}
+                      onChange={handleChange}
+                      required
+                      data-validation-required-message="Please enter a message."></textarea>
+                    <p className="help-block text-danger"></p>
+                  </div>
                 </div>
-              </div>
-              <br />
-              <div className="form-group"><button className="btn btn-primary btn-xl" id="sendMessageButton"
-                type="submit">Send</button></div>
-            </form>
+                <br />
+                <div className="form-group">
+                  <button 
+                    className="btn btn-primary btn-xl" 
+                    id="sendMessageButton"
+                    type="submit"
+                  >
+                    Send
+                  </button>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
-  </>
-);
+      </section>
+  );
+} 
 
 export default ContactForm; 
