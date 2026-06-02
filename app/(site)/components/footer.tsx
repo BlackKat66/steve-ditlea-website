99dc97b4fa13917816c524f1349afa899fba7743
BlackKat66 <kyeboa90909@outlook.com>
2026-06-01 20:03:21 -0400

    test
    test

--- app/(site)/components/footer.tsx
+++ app/(site)/components/footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => (
     <ContactForm />
     <div className="copyright py-4 text-center text-white">
     <div className="container text-medium">
-        Copyright © 2026 Steve Ditlea <br />
+        Copyright© 2026 Steve Ditlea <br />
         <a className="text-white" href="https://web.archive.org/web/20230425054929/http://www.tedrichards.net/" target="_blank">Web site design by Ted Richards</a>
     </div>
   </div>
